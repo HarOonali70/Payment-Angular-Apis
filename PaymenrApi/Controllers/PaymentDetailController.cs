ef13e48ebc7780e1ea4f9684f97c21ab026838d1
Asif Hameed <v-asif.hameed@imaginera.co.uk>
2022-06-03 05:27:28 +0500

    Add project files.

---
+++ PaymenrApi/Controllers/PaymentDetailController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymenrApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaymenrApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        private readonly PaymentDetailContext _context;
+
+        public PaymentDetailController(PaymentDetailContext context) { 
+            _context = context; 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetails>>> GetPaymentDetails() 
+        {
+            try {
+                return await _context.PaymentDetails.ToListAsync();
+            }
+            catch (Exception ex) { 
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<PaymentDetails>>> PostPaymentDetails(PaymentDetails paymentDetails) 
+        {
+            _context.PaymentDetails.Add(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetails", new { id = paymentDetails.PaymentDetailId }, paymentDetails);
+        }
+        [HttpPut]
+        public async Task<ActionResult> PutPaymentDetails(int id,PaymentDetails paymentDetails) 
+        {
+            if (id != paymentDetails.PaymentDetailId) 
+            {
+                    return BadRequest();
+            }
+            try {
+                _context.PaymentDetails.Update(paymentDetails).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetPaymentDetails", new { id = paymentDetails.PaymentDetailId }, paymentDetails);
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeletePaymentDetails(int id) 
+        {
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+            if (paymentDetail != null)
+            {
+                _context.PaymentDetails.Remove(paymentDetail);
+                _context.SaveChangesAsync();
+                return NoContent();
+            }
+            else {
+                return BadRequest();
+            }
+        }
+    } 
+}
